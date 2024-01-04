fa6fb51f1ea7fad952d9e71984ff244bc3ea7213
MuratYucedag <yucedagmurat23@gmail.com>
2024-01-04 21:51:27 +0300

    ....

---
+++ Services/Catalog/MyAkademiECommerce.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MyAkademiECommerce.Services.Catalog.Dtos.CategoryDtos;
+using MyAkademiECommerce.Services.Catalog.Dtos.ProductDtos;
+using MyAkademiECommerce.Services.Catalog.Models;
+
+namespace MyAkademiECommerce.Services.Catalog.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Category, ResultCategoryDto>();
+            CreateMap<ResultCategoryDto, Category>();
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+
+            CreateMap<Product,ResultProductDto>().ReverseMap();
+            CreateMap<Product,CreateProductDto>().ReverseMap();
+            CreateMap<Product,UpdateProductDto>().ReverseMap();
+        }
+    }
+}
